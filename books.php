68dca7271c073694e487c84b1cba5ecf23f1b28b
Daniel Baird <oldschoolskill19@gmail.com>
2021-01-19 18:47:23 -0700

    Uploading Json book list

---
+++ books.php
@@ -0,0 +1,5 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+?>
+{"books-read":[{"Title":"Magic of Thinking Big","Author":"David J. Schwartz"},{"Title":"The Compound Effect", "Author":"Darren Hardy"},
+{"Title":"Atomic Habits", "Author":"James Clear"}]}
