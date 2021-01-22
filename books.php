106b53e53a8e135806665903fef81f12d9fbec48
Daniel Baird <oldschoolskill19@gmail.com>
2021-01-21 18:13:16 -0700

    Testing Azure update

--- books.php
+++ books.php
@@ -2,4 +2,4 @@
 header("Access-Control-Allow-Origin: *");
 ?>
 {"books-read":[{"Title":"Magic of Thinking Big","Author":"David J. Schwartz"},{"Title":"The Compound Effect", "Author":"Darren Hardy"},
-{"Title":"Atomic Habits", "Author":"James Clear"}]}
+{"Title":"Atomic Habits", "Author":"James Clear"},{"Title":"Atomic Habits", "Author":"James Clear"}]}
